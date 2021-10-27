263a0076bd5cf41d2dc4907ec93f7fc2aa956301
6kimminjun <55810762+6kimminjun@users.noreply.github.com>
2021-10-27 00:06:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from google.cloud import datastore
 from flask import Flask, request
 import json
 import constants
-import lodging
-import guest
+import loads
+import boats
 
 app = Flask(__name__)
-app.register_blueprint(lodging.bp)
-app.register_blueprint(guest.bp)
+app.register_blueprint(loads.bp)
+app.register_blueprint(boats.bp)
 
 @app.route('/')
 def index():
-    return "Please navigate to /lodgings to use this API"
+    return "Please navigate to /boats to use this API"
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
